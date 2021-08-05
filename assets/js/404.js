439f7ad71315961225a1368540337f3dcc562e97
adithyarao3103 <adithyarao3132001@gmail.com>
2021-08-05 12:38:39 +0530

    minor ui changes

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,7 +1,7 @@
 function goback(i){
 	document.getElementById('back').innerHTML = "Taking you back to safety in 00:0" + i;
 	if (i==0) {
-		window.history.back();
+		window.location.href = 'index.html';
 	}
 	else{
 		setTimeout(function(){goback(i-1);},1000);
@@ -11,7 +11,7 @@ function goback(i){
 window.addEventListener('load',() =>{
 	console.log('start');
 	document.getElementById('back').style.opacity = 1;
-	setTimeout(function(){goback(4);},1000);
+	setTimeout(function(){goback(9);},1000);
 });
 
 
