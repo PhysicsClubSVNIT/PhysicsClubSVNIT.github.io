bbd88c60885163b6738e17df7c9687a960479c44
adithyarao3103 <adithyarao3132001@gmail.com>
2021-08-07 20:07:54 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-bgs = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg'];
+bgs = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg','7.jpg', '9.jpg', '11.jpg', '12.jpg'];
 
 particlesJS('particles',{
 	"particles": {
